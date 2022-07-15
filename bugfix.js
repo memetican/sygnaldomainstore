6a674d1366440ec7bd25fb5ff138adab4a32f1f7
Michael Wells <info@sygnal.com>
2022-07-15 17:46:31 +1200

    Add project files.

---
+++ bugfix.js
@@ -0,0 +1,28 @@
+
+applyFix();
+
+function applyFix()
+{
+
+  var fixApplied = document.body.getAttribute('fixApplied') != null;
+
+  // Run once
+  if(fixApplied) {
+    console.log('already run');
+    return;
+  }
+  
+  var pageText = document.body.innerHTML;
+
+  document.body.innerHTML = pageText.replace(
+    /\$([\d,]+(?:\.\d+)?)/g,
+    function (string, c1) {
+
+      return `$${c1} nzd`;
+
+    });
+
+  // Mark as fix applied
+  document.body.setAttribute('fixApplied','yes');
+
+}
